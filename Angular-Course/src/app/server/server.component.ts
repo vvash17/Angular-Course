a0f71fab31553ee3771109bd0a6a473804c0b2fc
vvash17 <vvash17@freeuni.edu.ge>
2021-06-14 13:40:22 +0400

    simple directive workarounds added, finished sample project moving to new one (from course milestones)

--- Angular-Course/src/app/server/server.component.ts
+++ Angular-Course/src/app/server/server.component.ts
@@ -2,10 +2,23 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [
+    `.online{
+      color: white;
+    }`
+  ]
 })
 
 export class ServerComponent {
   serverId = 'somth';
   sertverStatus = 'offline';
+
+  constructor() {
+    this.sertverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor(){
+    return this.sertverStatus === 'online' ? 'green' : 'red';
+  }
 }
--- Angular-Course/src/app/servers/servers.component.ts
+++ Angular-Course/src/app/servers/servers.component.ts
@@ -3,12 +3,22 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: [
+    `.logStyle{
+      background-color: wheat;
+    }
+    `
+  ]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Something';
+  serverCreated = true;
+  servers = ['Testserver'];
+  displayStatus = false;
+  log = [];
 
   constructor() {
     setTimeout(() => {
@@ -19,8 +29,15 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  changeDisplayStatus() {
+    this.displayStatus = !this.displayStatus;
+    this.log.push(new Date().toDateString());
+  }
+
   onCreateServer() {
     this.serverCreationStatus = 'New server was created! server name is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName(event: any) {
